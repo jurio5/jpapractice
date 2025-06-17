cc021d76cc73014a0840a881d8d281336b4c3fbe
jurio5 <sia5492@naver.com>
2025-06-18 01:20:54 +0900

    [feat] 관계형 DB를 객체 설계에 녹이는 방식, 객체 그래프 탐색이 불가능하며 이는 객체 지향적인 설계가 아님

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,7 +19,7 @@ public class Order {
     private Long id;
 
     @Column(name = "MEMBER_ID")
-    private Long memberId;
+    private Long memberId; // 관계형 DB를 객체에 맞춘 설계 , 객체 그래프 탐색이 불가능, 이는 객체 지향적이지 않음
 
     private LocalDateTime orderDate;
 
