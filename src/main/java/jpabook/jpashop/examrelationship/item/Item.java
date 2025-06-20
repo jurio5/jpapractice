e7b7e5672e84363c7fdd72540fdf0bcd9479bb9c
jurio5 <sia5492@naver.com>
2025-06-21 01:06:10 +0900

    [feat] 물리 모델링 전략 (단일 테이블, 서브타입 테이블(비추천))

--- src/main/java/jpabook/jpashop/examrelationship/item/Item.java
+++ src/main/java/jpabook/jpashop/examrelationship/item/Item.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn
-public class Item {
+//@Inheritance(strategy = InheritanceType.JOINED) // 조인 전략
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일 테이블 전략
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 서브타입 테이블 전략
+@DiscriminatorColumn // 단일 테이블 전략 같은 경우 이 어노테이션이 없어도 자동으로 `DTYPE`을 생성, 항상 있는게 좋음
+public abstract class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
