84aadf4ed3e31ad9d79d19e977b7bdb71fe076cf
jurio5 <sia5492@naver.com>
2025-06-22 02:18:42 +0900

    [feat] softDelete, hardDelete 베스트 프랙틱스

--- src/main/java/jpabook/jpashop/cascade/Child.java
+++ src/main/java/jpabook/jpashop/cascade/Child.java
@@ -15,6 +15,8 @@ public class Child {
 
     @ManyToOne
     // Hard Delete 방식, Review 가 있고, 그 Review 에 대해 좋아요 기능이 있다면, 리뷰가 삭제 시 그 리뷰의 좋아요도 함께 삭제되는 상황과 같을 때 사용
+    // 그러나 단순하게 접근하는 것이 중요, Review 를 soft delete 한다고 가정할 때
+    // 컬럼을 update 처리(변경)하고, 좋아요는 Delete(삭제)하는 것을 하나의 트랜잭션에 묶어서 처리하는 것 처럼
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "PARENT_ID")
     private Parent parent;
