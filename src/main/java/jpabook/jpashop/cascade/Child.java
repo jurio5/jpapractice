edd6d5d2416a6ae0d7af1f7cef1c28b12fc9abb5
jurio5 <sia5492@naver.com>
2025-06-22 02:16:23 +0900

    [feat] HardDelete 방식

--- src/main/java/jpabook/jpashop/cascade/Child.java
+++ src/main/java/jpabook/jpashop/cascade/Child.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.cascade;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 public class Child {
@@ -12,6 +14,8 @@ public class Child {
     private String name;
 
     @ManyToOne
+    // Hard Delete 방식, Review 가 있고, 그 Review 에 대해 좋아요 기능이 있다면, 리뷰가 삭제 시 그 리뷰의 좋아요도 함께 삭제되는 상황과 같을 때 사용
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "PARENT_ID")
     private Parent parent;
 
