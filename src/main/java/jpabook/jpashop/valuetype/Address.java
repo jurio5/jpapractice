d78ed895772c5f1adf029e170213066a63183382
jurio5 <sia5492@naver.com>
2025-06-24 00:46:24 +0900

    [feat] 복합 값 타입 객체에선 필드 명 역시 변경할 수 있음

--- src/main/java/jpabook/jpashop/valuetype/Address.java
+++ src/main/java/jpabook/jpashop/valuetype/Address.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.valuetype;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Address {
     private String city;
+    @Column(name = "STREET")
     private String street;
     private String zipcode;
 }
