bd8e730ef3ad63319b688524554f84c7b23d212a
jurio5 <sia5492@naver.com>
2025-06-24 03:30:18 +0900

    [feat] 기본 값 컬렉션 타입의 문제점 (PK가 없이 FK로 유지되는 컬렉션이기 때문에 쓰기 작업의 문제점이 있음)

--- src/main/java/jpabook/jpashop/valuetype/Member.java
+++ src/main/java/jpabook/jpashop/valuetype/Member.java
@@ -5,6 +5,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -30,4 +35,12 @@ public class Member {
             @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
     })
     private Address workAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 }
