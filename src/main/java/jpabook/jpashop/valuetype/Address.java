88db87c8ffa0c4c4dfb33c5eaebb60d318ff8938
jurio5 <sia5492@naver.com>
2025-06-24 02:01:23 +0900

    [feat] 객체를 불변 객체로 만드는 베스트 프랙틱스

--- src/main/java/jpabook/jpashop/valuetype/Address.java
+++ src/main/java/jpabook/jpashop/valuetype/Address.java
@@ -2,17 +2,28 @@ package jpabook.jpashop.valuetype;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Embeddable
 @Getter
-@Setter
-@NoArgsConstructor
-public class Address {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public final class Address {
     private String city;
     @Column(name = "STREET")
     private String street;
     private String zipcode;
+
+    @Builder(access = AccessLevel.PUBLIC, toBuilder = true)
+    private Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    public Address withCity(String city) {
+        return new Address(city, this.street, this.zipcode);
+    }
 }
