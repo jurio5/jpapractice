a502a7f2547b0db48dee42b8968a4ca4c4c55909
jurio5 <sia5492@naver.com>
2025-06-24 23:38:18 +0900

    [feat] JPA 에서 지원하는 표준 문법인 Criteria의 문제점 (복잡하고 어렵다)

--- src/main/java/jpabook/jpashop/jpql/JPQLMain.java
+++ src/main/java/jpabook/jpashop/jpql/JPQLMain.java
@@ -1,6 +1,9 @@
 package jpabook.jpashop.jpql;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import jpabook.jpashop.domain.Member;
 
 import java.util.List;
@@ -10,6 +13,19 @@ public class JPQLMain {
     EntityManager em;
 
     public static void main(String[] args) {
+    }
+
+    private static void criteria() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+        Root<Member> m = query.from(Member.class);
+
+        CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+        List<Member> resultList = em.createQuery(cq).getResultList();
+    }
+
+    private static void jpqlFind() {
         List<Member> result = em.createQuery("select m from Member m where m.name like '%kim%'", Member.class)
                 .getResultList();
     }
