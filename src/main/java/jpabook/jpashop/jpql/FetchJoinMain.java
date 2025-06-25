21aa335aa77569dff638232c944f2418f4c7a207
jurio5 <sia5492@naver.com>
2025-06-26 03:12:28 +0900

    [feat] 벌크 연산 사용 방법 및 주의사항

--- src/main/java/jpabook/jpashop/jpql/FetchJoinMain.java
+++ src/main/java/jpabook/jpashop/jpql/FetchJoinMain.java
@@ -28,6 +28,42 @@ public class FetchJoinMain {
         em.flush();
         em.clear();
 
+        String sql = "update Member m set m.age = 20";
+        // 이 때 flush() 호출 (DB 에는 age 컬럼이 20으로 변경되어 있음)
+        int resultCount = em.createQuery(sql, Member.class)
+                .executeUpdate();
+
+        System.out.println("member1.getAge() = " + member1.getAge()); // 호출 시 age 는 0으로 출력
+        // 이유는 쓰기 SQL 에서 DB 로 쿼리만 날렸을 뿐 영속성 컨텍스트 내부에 있는 age 값은 그대로임
+
+        em.clear(); // 영속성 컨텍스트 초기화 후
+
+        Member findMember = em.find(Member.class, member1.getId());
+        System.out.println("findMember.getAge() = " + findMember.getAge()); // 다시 DB 에서 영속성 컨텍스트로 데이터를 가져온 뒤 조회 -> 20 정상 호출
+
+        System.out.println("resultCount = " + resultCount);
+    }
+
+    private static void namedQuery() {
+        Team teamA = new Team();
+        teamA.setName("팀A");
+        em.persist(teamA);
+
+        Team teamB = new Team();
+        teamB.setName("팀B");
+        em.persist(teamB);
+
+        Member member1 = new Member();
+        member1.setTeam(teamA);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setTeam(teamB);
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
         List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
                 .setParameter("name", "회원1")
                 .getResultList();
